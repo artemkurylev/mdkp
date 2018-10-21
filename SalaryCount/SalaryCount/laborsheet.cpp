b2a71f35a14347a888ea6c31488c8653d79aef4d
Artem Kurylev <artembonapart@gmail.com>
2018-10-21 09:49:30 +0300

    Добавлен класс графика учета труда

---
+++ SalaryCount/SalaryCount/laborsheet.cpp
@@ -0,0 +1,12 @@
+#include "laborsheet.h"
+
+LaborSheet::LaborSheet(QObject *parent)
+    : DbRecord(parent)
+{
+
+}
+
+LaborSheet::~LaborSheet()
+{
+
+}
---
+++ SalaryCount/SalaryCount/laborsheet.h
@@ -0,0 +1,18 @@
+#ifndef LABORSHEET_H
+#define LABORSHEET_H
+
+#include "dbrecord.h"
+
+class LaborSheet : public DbRecord
+{
+    Q_OBJECT
+
+public:
+    LaborSheet(QObject *parent);
+    ~LaborSheet();
+
+private:
+    
+};
+
+#endif // LABORSHEET_H
