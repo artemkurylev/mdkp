a9529b585b26a98ba6c94b9a7852639c916ccc14
Pashulka <you@example.com>
2018-10-24 13:33:46 +0300

    В классе BookKeeper уточнены прототипы функций calcPerHoursSalary и calcPerMonthSalary.

--- SalaryCount/SalaryCount/BookKeeper.cpp
+++ SalaryCount/SalaryCount/BookKeeper.cpp
@@ -5,3 +5,14 @@ bool BookKeeper::calcSalary(const LaborSheet & laborSheet)
 {
 	return false;
 }
+
+static float BookKeeper::calcPerHoursSalary(float hourSalary, int hours)
+{
+	
+}
+
+static float BookKeeper::calcPerMonthSalary(float monthSalary, int days, int workDays)
+{
+	
+}
+
