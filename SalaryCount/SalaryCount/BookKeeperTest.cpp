38929b03901d925a8630c152c718fdd1e59b7ac6
Ismodon <ismometrical@gmail.com>
2018-10-24 13:14:04 +0300

    Created test class for methods in Bookkeeper class with unimplemented tests.

---
+++ SalaryCount/SalaryCount/BookKeeperTest.cpp
@@ -0,0 +1,30 @@
+#include "BookKeeperTest.h"
+
+
+
+BookKeeperTest::BookKeeperTest(QObject *parent)
+	: QObject(parent)
+{
+	
+
+}
+
+
+
+BookKeeperTest::~BookKeeperTest(void)
+{
+}
+
+void BookKeeperTest::testCalSalary(void)
+{
+	
+}
+
+void BookKeeperTest::testCalcPerHoursSalary(void)
+{
+}
+
+void BookKeeperTest::testCalcPerMonthSalary(void)
+{
+}
+
