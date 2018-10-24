11ab8338132cdf7e04b6f2324ba601e725d1d943
Artem Kurylev <artembonapart@gmail.com>
2018-10-24 15:13:06 +0300

    Изменено название поля grid на _grid

--- SalaryCount/SalaryCount/dutychart.cpp
+++ SalaryCount/SalaryCount/dutychart.cpp
@@ -4,14 +4,14 @@ DutyChart::DutyChart(QObject *parent)
     : DbRecord(parent)
 {
 	// создать типичный график 5\2
-	grid.clear();
-	grid.reserve(7);
+	_grid.clear();
+	_grid.reserve(7);
 	for(int i=0 ; i<7 ; ++i )
 	{
 		if (i < 5)
-			grid.push_back(Mark::Type::USUAL);
+			_grid.push_back(Mark::Type::USUAL);
 		else
-			grid.push_back(Mark::Type::HOLIDAY);
+			_grid.push_back(Mark::Type::HOLIDAY);
 	}
 }
 
