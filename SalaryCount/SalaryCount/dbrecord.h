46c07c9158f8080e76a23e03dd3f0345332fa56f
me-denis0w <hello-world-now@yandex.ru>
2018-10-29 05:32:54 +0400

    Переименованы методы соответствии с терминологией проекта и добавлены документирующие комментарии

--- SalaryCount/SalaryCount/dbrecord.h
+++ SalaryCount/SalaryCount/dbrecord.h
@@ -10,16 +10,26 @@ class DbRecord : public QObject
 public:
     DbRecord(QObject *parent);
 
-    ~DbRecord();
+   ~DbRecord();
 
+	/*! Запросить запись из БД [SELECT] (обновить локальную копию)
+	*/
     virtual bool fetch() = 0;
     
+	/*! Задать данные записи, не отправляя из в БД (обновить локальную копию)
+	*/
     virtual bool set() = 0;
     
+	/*! Проверить данные записи на корректность (полезно перед отправкой в БД)
+	*/
     virtual bool validate() const = 0;
     
+	/*! Отправить запись в БД [UPDATE]
+	*/
     virtual bool update() const = 0;
 
+	/*! Возвращает ID [Primary Key] записи в БД
+	*/
     int id() const;
 
     /*! Строка для запроса к базе данных MYSQL */
