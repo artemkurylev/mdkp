b57e2f121432032966c1652196bf5ef60e912a8a
Artem Kurylev <artembonapart@gmail.com>
2018-10-31 12:26:29 +0300

    Добавлена реализация метода fetch класса сотрудника, исправлен запрос на создание таблицы в связи с измененными данными о сотруднике

--- SalaryCount/SalaryCount/employee.cpp
+++ SalaryCount/SalaryCount/employee.cpp
@@ -12,7 +12,7 @@ bool Employee::createDbTable()
 {
     if(DbManager::manager().checkConnection()){
         QSqlQuery* query = DbManager::manager().makeQuery();
-        if(query->exec("CREATE TABLE IF NOT EXISTS `employee` (`id` INT(11) NOT NULL AUTO_INCREMENT, `fio` CHAR(30) NOT NULL, `phone_number` CHAR(20),`hire_date` DATE,`dutychart_id` INT(11), PRIMARY KEY(`id`))"))
+        if(query->exec("CREATE TABLE IF NOT EXISTS `employee` (`id` INT(11) NOT NULL AUTO_INCREMENT, `fio` CHAR(30) NOT NULL, `phone_number` CHAR(20),`inn` INT(10),`hire_date` DATE,`dutychart_id` INT(11),`next_dutychart_id` INT(11),`next_dutychart_since` DATE, PRIMARY KEY(`id`))"))
             return true;
         else{
             QString s = query->lastError().text();
@@ -22,6 +22,41 @@ bool Employee::createDbTable()
         delete query;
     }
     else{
+        return false;
+    }
+}
+bool Employee::fetch()
+{
+    if(DbManager::manager().checkConnection())
+    {
+        QSqlQuery* query = DbManager::manager().makeQuery();
+
+        query->prepare("SELECT * FROM `employee` WHERE `id` = :id");
+        int id = this->id();
+        query->bindValue(":id",id);
+        if(query->exec())
+        {
+            if(query->next())
+            {
+                _fio = query->value(1).toString();
+                _phoneNumber = query->value(2).toString();
+                _INN = query->value(3).toInt();
+                _hireDate = query->value(4).toDate();
+                _currentDutyChart = query->value(5).toInt();
+                _nextDutyChart = query->value(6).toInt();
+                _nextDutyChartSince = query->value(7).toDate();
+            }
+        }
+        else
+        {
+            QString s = query->lastError().text();
+            s+="as";
+            return false;
+        }
+        delete query;
+    }
+    else
+    {
         return false;
     }
 }
