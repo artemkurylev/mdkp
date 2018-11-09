f8338724a4c945499a3dd3008bf65d18ceafae26
Sereja Kalinkin <IKaSeI@yandex.ru>
2018-11-10 02:39:13 +0400

    Проведен рефакторинг, для перехода по страницам нужен только одно подключение
    композиция класса страницы графиков
    создано окно, уведомляющее о незаконченном редактировании элементов
    сорян за неатомарность

--- SalaryCount/SalaryCount/salarycount.cpp
+++ SalaryCount/SalaryCount/salarycount.cpp
@@ -12,7 +12,36 @@
 {
     ui.setupUi(this);
 
-    DbManager& manager = DbManager::manager();
+    initialDBManager();
+	this->editState = false;
+	
+	// запуск тестирования
+	QTest::qExec( new DirectiveGeneratorTest(0) , NULL , NULL);
+
+
+	//
+	this->dutyChart = new salarycountDutyChart(&this->ui);
+	connect(this->dutyChart,SIGNAL(changeState(bool)),this,SLOT(rememberState(bool)));
+	connect(this,SIGNAL(saveChanges()),this->dutyChart,SLOT(saveNewDutyChart()));
+	connect(this,SIGNAL(cancelChanges()),this->dutyChart,SLOT(cancelNewDutyChart()));
+
+	//постраничный переход
+	ui.stackedWidget->setCurrentIndex(0);//устанавлиаем видимость на странице с сотрудниками
+	this->currentAction = ui.EmployeeListAction;
+	this->currentAction->setEnabled(false);
+
+	connect(ui.CompanyMenu,SIGNAL(triggered(QAction*)), this,SLOT(showPage(QAction*)));
+}
+
+SalaryCount::~SalaryCount()
+{
+
+}
+
+
+void SalaryCount::initialDBManager()
+{
+	DbManager& manager = DbManager::manager();
     if(manager.checkConnection())
     {
         //Создание таблиц
@@ -31,254 +60,64 @@
     {
 
     }
-	
-	// запуск тестирования
-	QTest::qExec( new DirectiveGeneratorTest(0) , NULL , NULL);
 
-	//заполнение шаблона редактирования графика
-	for(int i=0; i<7;++i)
-	{
-		QComboBox* combo = new QComboBox();
-
-		QTextCodec* codec = QTextCodec::codecForLocale();
-
-		combo->insertItem(0, codec->toUnicode("Выходной"));
-		combo->insertItem(1, codec->toUnicode("Рабочий"));
-
-		combo->setCurrentIndex(1);
-
-		}
-	}
-	QTest::qExec( new DirectiveGeneratorTest(0) , NULL , NULL);
-		ui.DutyChartMarksEdit->setCellWidget(i,0,combo);
-	}
-	
-	ui.saveDutyChartBtn->setEnabled(true);
-	ui.cancelDutyChartBtn->setEnabled(true);
-
-	connect(ui.addDutyChart,SIGNAL(pressed()), this,SLOT(addDutyChart()));
-	connect(ui.payFormChoice,SIGNAL(currentIndexChanged(int)), this,SLOT(changePayForm(int)));
-	connect(ui.ExitAction,SIGNAL(triggered()), this,SLOT(close()));
-
-	//постраничный переход
-	this->currentAction = ui.EmployeeListAction;
-	this->currentAction->setEnabled(false);
-
-	connect(ui.EmployeeListAction,SIGNAL(triggered()), this,SLOT(showEmployeesPage()));
-	connect(ui.LaborSheetAction,SIGNAL(triggered()), this,SLOT(showLaborSheetsPage()));
-	connect(ui.DutyCharAction,SIGNAL(triggered()), this,SLOT(showDutyChartsPage()));
-	connect(ui.HireDirectiveAction,SIGNAL(triggered()), this,SLOT(showHireDirectivesPage()));
+	//return??
 }
 
-SalaryCount::~SalaryCount()
+bool SalaryCount::isEditable()
 {
-
-}
-
-
-void SalaryCount::addDutyChart()
-{
-	ui.dutyChartEdit->setEnabled(true);
-}
-
-void SalaryCount::changePayForm(int index)
-{
-	if(index)
+	if(this->editState)
 	{
-		ui.workTimeEdit->setEnabled(true);
-	}
-	else
-	{
-		ui.workTimeEdit->setEnabled(false);
-	}
-}
-
-void SalaryCount::saveNewDutyChart()
-{
-	QList<Mark> marks;
-
-	for(int i=0;i<7;++i)
-	{
-		QComboBox* combo = (QComboBox*)ui.DutyChartMarksEdit->cellWidget(i,0);
-		Mark m(0);
-		int markValue = combo->currentIndex();
-
-		if(ui.payFormChoice->currentIndex()==0)
+		QTextCodec* c = QTextCodec::codecForLocale();
+		QMessageBox::StandardButton btn = QMessageBox::question(this, QString(c->toUnicode("Данные не сохранены")), 
+																		QString(c->toUnicode("Сохранить изменения перед выходом?")), 
+																		QMessageBox::Yes | QMessageBox::No | QMessageBox::Cancel);
+		switch(btn)
 		{
-			if(markValue==0) 
-			{
-				m.setBase(Mark.Type.ATTENDS);
-			}
+			case QMessageBox::StandardButton(QMessageBox::Yes):
+				emit saveChanges();
+				return false;
 
-			if(markValue==1) 
-			{
-				m.setBase(Mark::Type.HOLIDAY);
-			}
-		}
-		else
-		{
-			
-			if(markValue==0) 
-			{
-				m.setBase(ui.workTimeEdit->time().hour());
-			}
+			case QMessageBox::StandardButton(QMessageBox::No):
+				emit cancelChanges();
+				return true;
 
-			if(markValue==1) 
-			{
-				m.setBase(Mark::Type.HOLIDAY);
-			}
+			case QMessageBox::StandardButton(QMessageBox::Cancel):
+			default:
+				return true;
 		}
-		marks.append(m);
 	}
-	DutyChart dch(marks);
-	dch.set();
-		
-		int mark_value = combo->currentIndex();
-		int choice_value = ui.payFormChoice->currentIndex();
 
+	return false;
+}
 
-		Mark m(mark_value);
-		DutyChart dch(marks);
-		dch.set();
+/*!
+*\
+*/
+void SalaryCount::showPage(QAction* actionEmited)
+{
+	if(!isEditable())
+	{
+		QString namePage = actionEmited->whatsThis();
+
+		this->currentAction->setEnabled(true);
+		this->currentAction = actionEmited;
+		this->currentAction->setEnabled(false);
+
+		showStackedItem(namePage);
 	}
 }
 
 /*!
 *\
 */
-void SalaryCount::cancelNewDutyChart()
+void SalaryCount::showStackedItem(QString namePage)
 {
-
-
-}
-
-/*!Переход на страницу учета сотрудников
-*\
-*/
-void SalaryCount::showEmployeesPage()
-{
-	int indexPage = 0;
-	QString namePage = QString("EmployeesPage");
-
-	this->currentAction->setEnabled(true);
-	this->currentAction = ui.EmployeeListAction;
-	this->currentAction->setEnabled(false);
-
-	showStacketItem(indexPage, namePage);
-}
-
-/*!Переход на страницу табелей учета труда
-*\
-*/
-void SalaryCount::showLaborSheetsPage()
-{
-	int indexPage = 1;
-	QString namePage = QString("LaborSheetsPage");
-
-	this->currentAction->setEnabled(true);
-	this->currentAction = ui.LaborSheetAction;
-	this->currentAction->setEnabled(false);
-
-	showStacketItem(indexPage, namePage);
-}
-
-/*!Переход на страницу графиков
-*\
-*/
-void SalaryCount::showDutyChartsPage()
-{
-	int indexPage = 2;
-	QString namePage = QString("DutyChartsPage");
-
-	this->currentAction->setEnabled(true);
-	this->currentAction = ui.DutyCharAction;
-	this->currentAction->setEnabled(false);
-
-	showStacketItem(indexPage, namePage);
-}
-
-/*!Переход на страницу приказов
-*\
-*/
-void SalaryCount::showHireDirectivesPage()
-{
-	int indexPage = 3;
-	QString namePage = QString("HireDirectivesPage");
-
-	this->currentAction->setEnabled(true);
-	this->currentAction = ui.HireDirectiveAction;
-	this->currentAction->setEnabled(false);
-
-	showStacketItem(indexPage, namePage);
-}
-
-/*!Переход на страницу учета сотрудников
-*\
-*/
-void SalaryCount::showEmployeesPage()
-{
-	int indexPage = 0;
-	QString namePage = QString("EmployeesPage");
-
-	this->currentAction->setEnabled(true);
-	this->currentAction = ui.EmployeeListAction;
-	this->currentAction->setEnabled(false);
-
-	showStacketItem(indexPage, namePage);
-}
-
-/*!Переход на страницу табелей учета труда
-*\
-*/
-void SalaryCount::showLaborSheetsPage()
-{
-	int indexPage = 1;
-	QString namePage = QString("LaborSheetsPage");
-
-	this->currentAction->setEnabled(true);
-	this->currentAction = ui.LaborSheetAction;
-	this->currentAction->setEnabled(false);
-
-	showStacketItem(indexPage, namePage);
-}
-
-/*!Переход на страницу графиков
-*\
-*/
-void SalaryCount::showDutyChartsPage()
-{
-	int indexPage = 2;
-	QString namePage = QString("DutyChartsPage");
-
-	this->currentAction->setEnabled(true);
-	this->currentAction = ui.DutyCharAction;
-	this->currentAction->setEnabled(false);
-
-	showStacketItem(indexPage, namePage);
-}
-
-/*!Переход на страницу приказов
-*\
-*/
-void SalaryCount::showHireDirectivesPage()
-{
-	int indexPage = 3;
-	QString namePage = QString("HireDirectivesPage");
-
-	this->currentAction->setEnabled(true);
-	this->currentAction = ui.HireDirectiveAction;
-	this->currentAction->setEnabled(false);
-
-	showStacketItem(indexPage, namePage);
-}
-
-void SalaryCount::showStacketItem(int indexPage, QString namePage)
-{
-	const QWidget* searchPage = ui.stackedWidget->widget(indexPage);
+	QWidget* searchPage = ui.stackedWidget->findChild<QWidget*>(namePage);
 
 	if(searchPage && namePage.compare( searchPage->accessibleName() ))
 	{
-		ui.stackedWidget->setCurrentIndex(indexPage);
+		ui.stackedWidget->setCurrentWidget(searchPage);
 	}
 	else
 	{
@@ -286,4 +125,12 @@ void SalaryCount::showStacketItem(int indexPage, QString namePage)
 		QMessageBox::critical(this,c->toUnicode(""), c->toUnicode("Страница не существует"));
 	}
 
+}
+
+/*!
+*\
+*/
+void SalaryCount::rememberState(bool state)
+{
+	this->editState = state;
 }
