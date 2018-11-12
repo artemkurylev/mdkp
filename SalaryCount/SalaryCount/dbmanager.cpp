0714acce196e033873586834b38694918a276fca
den1s0v <hello-lumen@ya.ru>
2018-11-12 12:49:30 +0300

    Автоматическое создание и чение INI-файла с конфигурацией подключения к БД

--- SalaryCount/SalaryCount/dbmanager.cpp
+++ SalaryCount/SalaryCount/dbmanager.cpp
@@ -1,5 +1,10 @@
 #include "dbmanager.h"
 
+struct DbConf loadDbConfig();
+#include <QDateTime>
+#include <QFile>
+#include <QSettings>
+
 DbManager::DbManager()
     : QObject(0)
 {
@@ -59,10 +64,14 @@ bool DbManager::checkConnection()
 {
     if(!singletonExists)
     {
-        if(false)
-            DbManager::globalManager = new DbManager("localhost","salarycount",3306,"root","root");
-        else
-            DbManager::globalManager = new DbManager("109.206.169.214","salary_count",81,"remote","!E3f5c712");
+		struct DbConf conf = loadDbConfig();
+		//QString hostName, dbName, userName, pass;
+		//int port;
+		DbManager::globalManager = new DbManager(conf.hostName,conf.dbName,conf.port,conf.userName,conf.pass);
+        //if(false)
+        //    DbManager::globalManager = new DbManager("localhost","salarycount",3306,"root","root");
+        //else
+        //    DbManager::globalManager = new DbManager("109.206.169.214","salary_count",81,"remote","!E3f5c712");
 			// test ports: cmd>telnet 109.206.169.214 81
         DbManager::singletonExists = 1;
     }
@@ -73,3 +82,52 @@ DbManager::~DbManager()
 
 }
 
+struct DbConf loadDbConfig()
+{
+	struct DbConf  dbConf;
+	QString fname = "dbconfig.ini";
+	QSettings s(fname, QSettings::IniFormat);
+	
+	if(!QFile::exists(fname))
+	{
+		//QString hostName, dbName, userName, pass;
+		//int port;
+		dbConf.hostName = "localhost";
+		dbConf.port = 3306;
+		dbConf.dbName = "salarycount";
+		dbConf.userName = "root";
+		dbConf.pass = "root"; // "our366Team";
+
+		// write 
+		s.beginGroup("db");
+		s.setValue("hostName", dbConf.hostName);
+		s.setValue("port", dbConf.port);
+		s.setValue("dbName", dbConf.dbName);
+		s.setValue("userName", dbConf.userName);
+		s.setValue("pass", dbConf.pass);
+		s.endGroup();
+
+		// write 
+		s.beginGroup("meta");
+		s.setValue("write_at", QDateTime::currentDateTime().toString("dd.MM.yyyy  hh:mm:ss"));
+		s.endGroup();
+	}
+	else
+	{
+		s.beginGroup("db");
+		dbConf.hostName = s.value("hostName", "localhost").toString();
+		dbConf.port = s.value("port", 3306).toInt();
+		dbConf.dbName = s.value("dbName", "salarycount").toString();
+		dbConf.userName = s.value("userName", "root").toString();
+		dbConf.pass = s.value("pass", "root").toString();
+		s.endGroup();
+
+		// read 
+		s.beginGroup("meta");
+		//s.setValue("write_at", QDateTime::currentDateTime().toString("dd.MM.yyyy  hh:mm:ss"));
+		s.setValue("read_at", QDateTime::currentDateTime().toString("dd.MM.yyyy  hh:mm:ss"));
+		s.endGroup();
+	}
+
+	return dbConf;
+}
