bf8b5de401ac2496991372ced841ea547f4aad33
den1s0v <hello-lumen@ya.ru>
2018-11-15 00:13:07 +0400

    Каркас метода openBillingPeriod, содержащий закрытие периода и открытие следующего

--- SalaryCount/SalaryCount/BookKeeper.cpp
+++ SalaryCount/SalaryCount/BookKeeper.cpp
@@ -8,9 +8,30 @@ bool BookKeeper::calcAwardFor(LaborSheet & laborSheet)
 
 	return false;
 }
+
 bool openBillingPeriod(BillingPeriod & billingPeriod)
 {
-	return false;
+	int bp_status = billingPeriod.status();
+	BillingPeriod & next_period = *billingPeriod.nextPeriod();
+	if( bp_status != BillingPeriod::OPEN &&		// НАЙДЕН способ обрщения к чужим enum`ам !
+		bp_status != BillingPeriod::MODIFIED &&
+		next_period.status() != BillingPeriod::NOT_OPENED )
+	{
+		// preconditions error
+		return false;
+	}
+
+	billingPeriod.close();
+	billingPeriod.update();
+
+	// расчитать з/п
+	// Для каждого табеля в текущем периоде...
+	// ... Вычислить зарплату за период по закрытому месячному табелю
+
+	next_period.open();
+	next_period.update();
+	
+	return true;
 }
 
 
