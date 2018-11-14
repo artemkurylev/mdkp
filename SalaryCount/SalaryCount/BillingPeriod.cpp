5fb528ecdc05a3558c186bc884a7f1cd9bb16083
den1s0v <hello-lumen@ya.ru>
2018-11-15 03:47:08 +0400

    Реализация методов update() и getCurrentPeriod() в классе BillingPeriod

--- SalaryCount/SalaryCount/BillingPeriod.cpp
+++ SalaryCount/SalaryCount/BillingPeriod.cpp
@@ -74,7 +74,30 @@ bool BillingPeriod::validate() const
 }
 bool BillingPeriod::update() const
 {
-    return false;
+	bool success = false;
+    if(DbManager::manager().checkConnection())
+    {
+        QSqlQuery* query = DbManager::manager().makeQuery();
+        query->prepare("UPDATE `billing_period` SET `start_date`=:start_date, `status`=:status WHERE  `id`=:id;");
+        query->bindValue(":start_date",this->_startDate);
+        query->bindValue(":status",this->_status);
+        query->bindValue(":id",this->_id);
+        if(query->exec())
+        {
+			success = true;
+		}
+        else
+        {
+            QString s = query->lastError().text();
+            s+="as";
+			s = query->executedQuery();
+        }
+        delete query;
+    }
+    else
+	{
+    }
+    return success;
 }
 int BillingPeriod::insert()
 {
@@ -87,10 +110,11 @@ int BillingPeriod::insert()
         query->bindValue(":status",this->_status);
         if(query->exec())
         {
-            query->prepare("SELECT id FROM `billing_period` WHERE `start_date` = :start_date");
-            query->bindValue(":start_date",this->_startDate);
-            if(query->exec() && query->next())
-                this->_id = query->value(0).toInt();
+            //query->prepare("SELECT id FROM `billing_period` WHERE `start_date` = :start_date");
+            //query->bindValue(":start_date",this->_startDate);
+			QVariant last_id = query->lastInsertId();
+            if(last_id.isValid())
+                this->_id = last_id.toInt();
                 insert_id = this->_id;
         }
         else
@@ -156,7 +180,7 @@ void BillingPeriod::set_modified()
 {
 	_status = MODIFIED;
 }
-BillingPeriod* BillingPeriod::getByDate(const QDate& date)
+/*static*/ BillingPeriod* BillingPeriod::getByDate(const QDate& date)
 {
 	BillingPeriod* billing_period = NULL;
     if(DbManager::manager().checkConnection())
@@ -172,7 +196,7 @@ BillingPeriod* BillingPeriod::getByDate(const QDate& date)
     }
     return billing_period;
 }
-long BillingPeriod::countEntries()
+/*static*/ long BillingPeriod::countEntries()
 {
     int counter = 0;
     if(DbManager::manager().checkConnection())
@@ -189,12 +213,27 @@ long BillingPeriod::countEntries()
     }
     return counter;
 }
-BillingPeriod* BillingPeriod::getByDate(const QDate& date)
+
+/*static*/ BillingPeriod* BillingPeriod::getCurrentPeriod()
 {
-    int counter = 0;
+	BillingPeriod* billing_period = NULL;
     if(DbManager::manager().checkConnection())
     {
         QSqlQuery* query = DbManager::manager().makeQuery();
+        query->prepare("SELECT * FROM billing_period WHERE status = :status");
+        query->bindValue(":status",OPEN);
+        if(query->exec() && query->next())
+        {
+            billing_period = new BillingPeriod(
+				query->value(0).toInt(),
+				query->value(1).toDate(),
+				(BillingPeriod::Status)query->value(2).toInt()
+				);
+        }
+		delete query;
+    }
+    return billing_period;
+}
 
         query->prepare("SELECT * FROM `billing_period` WHERE start_date = :date");
         query->bindValue(":date",date);
