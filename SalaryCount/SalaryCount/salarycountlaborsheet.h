53660ca09226c6105e514b233064950146b24e33
Artem Kurylev <artembonapart@gmail.com>
2018-11-15 00:20:27 +0300

    Добавил id табеля в таблицу

--- SalaryCount/SalaryCount/salarycountlaborsheet.h
+++ SalaryCount/SalaryCount/salarycountlaborsheet.h
@@ -3,6 +3,7 @@
 
 #include "delegateStates.h"
 #include "laborsheet.h"
+#include"BillingPeriod.h"
 #include<qtablewidget.h>
 class salarycountLaborSheet : public delegateStates
 {
@@ -16,6 +17,8 @@ private:
 
     Ui_SalaryCount *ui; // не самый при€тный способ, зато все видно из коробки
 	void showAllLabors();
+private slots:
+    void showLabor();
 };
 
 #endif // SALARYCOUNTLABORSHEET_H
