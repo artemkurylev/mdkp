593d7fa0fd75366149908178451a9e7d32866591
Sereja Kalinkin <IKaSeI@yandex.ru>
2018-11-16 05:35:57 +0400

    Исправлены коллизии со связью между страницей и главным окном

--- SalaryCount/SalaryCount/salarycount.cpp
+++ SalaryCount/SalaryCount/salarycount.cpp
@@ -55,7 +55,7 @@
 	QTest::qExec( &dir_gen_test , NULL , NULL);
 
 	//соединение со страницей создания графиков
-	this->dutyChart = new salarycountDutyChart(&this->ui,ui.DutyCharAction->objectName());
+	this->dutyChart = new salarycountDutyChart(&this->ui,ui.DutyCharAction->whatsThis());
     //Соедиенение со страницей табелей
     this->laborSheet = new salarycountLaborSheet(&this->ui,ui.LaborSheetAction->objectName());
 	connect(this, SIGNAL(showPage(QString)),this->dutyChart,SLOT(updateInfo(QString)));//обновить информацию на странице
@@ -121,11 +121,12 @@ bool SalaryCount::isEditable()
 		{
 			case QMessageBox::StandardButton(QMessageBox::Yes):
 				emit saveChanges();
-				return false;
+				return this->editState;
 
 			case QMessageBox::StandardButton(QMessageBox::No):
 				emit cancelChanges();
-				return true;
+
+				return this->editState;
 
 			case QMessageBox::StandardButton(QMessageBox::Cancel):
 			default:
