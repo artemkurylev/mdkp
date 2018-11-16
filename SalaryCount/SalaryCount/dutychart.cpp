c09025639f53fb832bbbb9d41eabbd28b0534860
Artem Kurylev <artembonapart@gmail.com>
2018-11-16 19:13:20 +0300

    Ну как бы получилось

--- SalaryCount/SalaryCount/dutychart.cpp
+++ SalaryCount/SalaryCount/dutychart.cpp
@@ -150,8 +150,8 @@ bool DutyChart::fetch()
                     while(query_m.next())
                     {
                         Mark m(query_m.value(1).toInt(),query_m.value(2).toInt(),query_m.value(3).toInt(),query_m.value(4).toInt(),query_m.value(5).toInt(),query_m.value(6).toInt());
-                        m.setId(query_m.value(0).toInt());
                         _grid.append(m);
+                        _grid[_grid.size() - 1].setId(query_m.value(0).toInt());
                     }
                     return true;
                 }
