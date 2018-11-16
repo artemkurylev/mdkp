6172ccb46435f676112466a4af27006862ce8e5b
Sereja Kalinkin <IKaSeI@yandex.ru>
2018-11-16 05:33:32 +0400

    Добавлен метод вывода пользовательских ошибок

--- SalaryCount/SalaryCount/delegateStates.cpp
+++ SalaryCount/SalaryCount/delegateStates.cpp
@@ -9,3 +9,9 @@ delegateStates::~delegateStates()
 {
 
 }
+
+void delegateStates::error_msg(const char* short_description, const char* text)
+{
+	QTextCodec* c = QTextCodec::codecForLocale();
+	QMessageBox::critical(NULL,c->toUnicode(short_description), c->toUnicode(text));
+}
