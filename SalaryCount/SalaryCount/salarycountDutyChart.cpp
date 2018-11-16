12d52d5899f3cfcf73208ef12d1d781d84bea578
Sereja Kalinkin <IKaSeI@yandex.ru>
2018-11-16 05:37:22 +0400

    Страница графиков готова, сорян за неатомарность

--- SalaryCount/SalaryCount/salarycountDutyChart.cpp
+++ SalaryCount/SalaryCount/salarycountDutyChart.cpp
@@ -8,7 +8,7 @@ salarycountDutyChart::salarycountDutyChart(Ui_SalaryCount* ui, QString name)
 	this->ui->editDutyChart->setEnabled(false);
 	this->ui->deleteDutyChart->setEnabled(false);
 
-	for(int i=0; i<7;++i)
+	for(int i=0; i<ui->DutyChartMarksEdit->rowCount();++i)
 	{
 		QComboBox* combo = new QComboBox();
 
@@ -30,10 +30,10 @@ salarycountDutyChart::salarycountDutyChart(Ui_SalaryCount* ui, QString name)
 	connect(ui->editDutyChart,SIGNAL(pressed()), SLOT(editDutyChart()));
 	connect(ui->cancelDutyChartBtn,SIGNAL(pressed()), SLOT(cancelNewDutyChart()));
 	connect(ui->saveDutyChartBtn,SIGNAL(pressed()),this,SLOT(saveNewDutyChart()));
-	connect(ui->deleteDutyChart,SIGNAL(pressed()), SLOT(deleteDutyChart(ui->dutyChartList->currentRow())));
+	connect(ui->deleteDutyChart,SIGNAL(pressed()), SLOT(deleteDutyChart()));
 
 	connect(ui->payFormChoice,SIGNAL(currentIndexChanged(int)), SLOT(changePayForm(int)));
-	connect(ui->saveDutyChartBtn,SIGNAL(currentRowChanged(int)), SLOT(showSelectedItem(int)));
+	connect(ui->dutyChartList,SIGNAL(currentRowChanged(int)), SLOT(showSelectedItem(int)));
 }
 
 salarycountDutyChart::~salarycountDutyChart()
@@ -78,14 +78,12 @@ void salarycountDutyChart::editDutyChart()
 */
 void salarycountDutyChart::cancelNewDutyChart()
 {
-	this->currentState = app_states::USUAL;
-	emit changeState(false);
-	clearFields();//clear fields
 	switchMode(app_states::USUAL);
 
-	if(ui->dutyChartList->count())
+	int row = ui->dutyChartList->currentRow();
+	if(row!=-1)
 	{ 
-		ui->dutyChartList->setCurrentRow(0);
+		showSelectedItem(row);
 	}
 	else
 	{
@@ -93,22 +91,28 @@ void salarycountDutyChart::cancelNewDutyChart()
 	}
 }
 
-void salarycountDutyChart::deleteDutyChart(int row)
+void salarycountDutyChart::deleteDutyChart()
 {
-	if(row!=-1)
+	int row = ui->dutyChartList->currentRow();
+
+	int countItems = ui->dutyChartList->count();
+	if(row>-1 && countItems>row)
 	{
 		int ID = ui->dutyChartList->item(row)->type();
 
-		DutyChart* curChart = new DutyChart( ID );
+		DutyChart curChart(ID);
 
-		if(/*curChart->del()*/0)
+		if(curChart.delet())
 		{//типа удалили из бд
-			ui->dutyChartList->setCurrentRow(row-1);
-			ui->dutyChartList->removeItemWidget(ui->dutyChartList->item(row));
+			if(countItems!=1 && row!=0) ui->dutyChartList->setCurrentRow(row-1);
+			if(countItems!=1 && row+1!=countItems) ui->dutyChartList->setCurrentRow(row+1);
+			if(countItems==1) ui->dutyChartList->setCurrentRow(-1);
+
+			delete ui->dutyChartList->takeItem(row);
 		}
 		else
 		{
-			//cообщили об ошибке
+			error_msg("Оопаньки","Ебать-Копать");//cообщили об ошибке
 		}
 	}
 }
@@ -119,30 +123,33 @@ void salarycountDutyChart::saveNewDutyChart()
 
 	if(obj->validate())
 	{//проверить данные
-		if(this->currentState == app_states::ADD)
-		{
-			saveNewEntries(obj);
-			return;
-		}
 
-		if(this->currentState == app_states::EDIT)
+		switch(this->currentState)
 		{
-			saveEditableEntries(obj);
+			case app_states::ADD:
+				saveNewEntries(obj);
+			return;
+
+			case app_states::EDIT:
+				saveEditableEntries(obj);
 			return;
 		}
 	}
 	else
 	{
-		//что-то сделать: выдать сообщение об ошибке
+		error_msg("Оопаньки","Ебать-Копать");//что-то сделать: выдать сообщение об ошибке
 	}
+
+	delete obj;
 }
 
 void salarycountDutyChart::saveNewEntries(DutyChart* obj)
 {
-	if(obj->insert())
+	int ID;
+	if((ID = obj->insert())!=-1 && obj)
 	{
 		//добавить значение в конец списка
-		QListWidgetItem *item = new QListWidgetItem(obj->name(), ui->dutyChartList, obj->id());
+		QListWidgetItem *item = new QListWidgetItem(obj->name(), ui->dutyChartList, ID);
 		ui->dutyChartList->addItem(item);
 
 		ui->dutyChartList->setCurrentRow(ui->dutyChartList->count()-1);
@@ -150,7 +157,7 @@ void salarycountDutyChart::saveNewEntries(DutyChart* obj)
 	}
 	else
 	{
-		//сообщить об ошибке	
+		error_msg("Оопаньки","Ебать-Копать");//сообщить об ошибке	
 	}
 }
 
@@ -159,10 +166,13 @@ void salarycountDutyChart::saveEditableEntries(DutyChart* obj)
 	if(obj->update())
 	{
 		switchMode(app_states::USUAL);
+
+		QListWidgetItem *item = ui->dutyChartList->currentItem();
+		if(item) item->setText(obj->name());
 	}
 	else
 	{
-		//сообщить об ошибке	
+		error_msg("Оопаньки","Ебать-Копать");//сообщить об ошибке	
 	}
 }
 
@@ -178,7 +188,7 @@ DutyChart* salarycountDutyChart::shapeDataObject()
 	QString name = ui->nameDutyChart->text();//занести имя
 	QDate ancDate = ui->startDate->date();//запомнить якорную дату
 	PayForm pf = (ui->payFormChoice->currentIndex()==0 ? PayForm::PER_MONTH : PayForm::PER_HOUR);//запомнить платежную форму
-	QTime* workTime = new QTime(ui->workTimeEdit->time());//в зависимости от платежной формы запомнить рабочее время
+	QTime workTime(ui->workTimeEdit->time());//в зависимости от платежной формы запомнить рабочее время
 
 	//запомнить отметки
 	QList<Mark> *ms = new QList<Mark>();
@@ -186,25 +196,30 @@ DutyChart* salarycountDutyChart::shapeDataObject()
 	{
 		QComboBox* combo = (QComboBox*)ui->DutyChartMarksEdit->cellWidget(i,0);
 
-		Mark *m;// = new Mark( () );
+		Mark *m = NULL;
 
-		/*if(pf==PayForm::PER_MONTH)
+		switch(pf)
 		{
-			m = new Mark( (combo->currentIndex()==1 ? Mark::Type::ATTENDS : Mark::Type::HOLIDAY) );
+			case PayForm::PER_MONTH:
+				m = new Mark( (combo->currentIndex()==1 ? 
+					Mark::Type::ATTENDS : Mark::Type::HOLIDAY) );
+				break;
+
+			case PayForm::PER_HOUR:
+				m = new Mark( NULL,NULL,(combo->currentIndex()==1 ? 
+					ui->workTimeEdit->time().hour() : Mark::Type::HOLIDAY),NULL );
+				break;
+
+			default:
+				throw new nullptr_t;
 		}
-
-		if(pf==PayForm::PER_HOUR)
-		{
-			m = new Mark( ui->workTimeEdit->time().hour() );
-		}
-
-		if(m[i].base() == Mark::Type.HOLIDAY)
-		{
-			QComboBox* combo = (QComboBox*)ui->DutyChartMarksEdit->cellWidget(i,0);
-			combo->setCurrentIndex(1);
-		}*/
+			
+		ms->append(*m);
+		delete m;
 	}
-	DutyChart* obj = new DutyChart(1);
+
+	DutyChart* obj = new DutyChart(name,*ms,ancDate,pf);
+	delete ms;
 
 	return obj;
 }
@@ -252,14 +267,18 @@ void salarycountDutyChart::updateInfo(QString name)
 
 		if(allEntries.count())
 		{
-			QList<int> keys = allEntries.keys();
+			QList<int> &keys = allEntries.keys();
+			QList<QString> &val = allEntries.values();
 			qSort(keys);
 
 			foreach(const int &iter, keys)
 			{
+				QString d = allEntries.value( iter );
 				QListWidgetItem *item = new QListWidgetItem(allEntries.value( iter ), ui->dutyChartList, iter);
 				ui->dutyChartList->addItem(item);
 			}
+
+			ui->dutyChartList->setCurrentRow(0);
 		}
 	}
 }
@@ -277,59 +296,51 @@ void salarycountDutyChart::parseDataObject(const DutyChart *obj)
 		ui->workTimeEdit->setTime(QTime(0,0));
 	}
 
-	QList<Mark> m = obj->grid();
-	for(int i=this->ui->DutyChartMarksEdit->rowCount()-1; i>=0; --i)
+	const QList<Mark> &m = obj->grid();
+
+	if(m.count()==ui->DutyChartMarksEdit->rowCount())
 	{
-		if(m[i].base() == Mark::Type::HOLIDAY)
+		for(int i=m.count()-1; i>=0; --i)
 		{
 			QComboBox* combo = (QComboBox*)ui->DutyChartMarksEdit->cellWidget(i,0);
-			combo->setCurrentIndex(1);
+			if(m[i].base() == Mark::Type::HOLIDAY)
+			{
+				combo->setCurrentIndex(1);
+			}
+			else
+			{
+				combo->setCurrentIndex(0);
+			}
 		}
 	}
 }
 
 void salarycountDutyChart::showSelectedItem( int row )
 {
-	this->ui->nameDutyChart->clear();
-	this->ui->startDate->setDateTime(QDateTime(QDate::currentDate()));
-	this->ui->workTimeEdit->setTime(QTime(0,0));
+	clearFields();
 
-	for(int i=this->ui->DutyChartMarksEdit->rowCount()-1; i>=0; --i)
+	if(row!=-1 && ui->dutyChartList->count() > row)
 	{
-		QComboBox* combo = (QComboBox*)ui->DutyChartMarksEdit->cellWidget(i,0);
-		
-		if(combo->count()>=2)
+		int ID = ui->dutyChartList->currentItem()->type();
+
+		DutyChart curChart( ID );
+
+		if(curChart.fetch())
 		{
-			combo->setCurrentIndex(1);
+			parseDataObject(&curChart);
 		}
-	}
+		else
+		{
+			error_msg("Оопаньки","Ебать-Копать");//TODO 
+			//error parse
+		}
 
-	if(this->ui->payFormChoice->count()>=1)
+		this->ui->editDutyChart->setEnabled(true);
+		this->ui->deleteDutyChart->setEnabled(true);
+	}
+	else
 	{
-		this->ui->payFormChoice->setCurrentIndex(0);
-	    if(row!=-1)
-	    {
-		    int ID = ui->dutyChartList->currentItem()->type();
-
-		    DutyChart* curChart = new DutyChart( ID );
-
-		    if(curChart->fetch())
-		    {
-			    parseDataObject(curChart);
-		    }
-		    else
-		    {
-			    //TODO 
-			    //error parse
-		    }
-
-		    this->ui->editDutyChart->setEnabled(true);
-		    this->ui->deleteDutyChart->setEnabled(true);
-	    }
-	    else
-	    {
-		    this->ui->editDutyChart->setEnabled(false);
-		    this->ui->deleteDutyChart->setEnabled(false);
-        }
-	}
+		this->ui->editDutyChart->setEnabled(false);
+		this->ui->deleteDutyChart->setEnabled(false);
+    }
 }
