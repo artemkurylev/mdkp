c98ffeea6c88a875169eb8cb748f8f484544f687
Artem Kurylev <artembonapart@gmail.com>
2018-11-18 16:58:25 +0400

    Добавил конструктор и метод updateInfo(пока заглушка)

--- SalaryCount/SalaryCount/salarycountdirectives.cpp
+++ SalaryCount/SalaryCount/salarycountdirectives.cpp
@@ -2,10 +2,15 @@
 
 salarycountDirectives::salarycountDirectives(Ui_SalaryCount* ui, QString name)
 {
-
+    this->setObjectName(name);
+	this->ui = ui;//не самый приятный способ
 }
 
 salarycountDirectives::~salarycountDirectives()
+{
+
+}
+void salarycountDirectives::updateInfo(QString name)
 {
 
 }
--- SalaryCount/SalaryCount/salarycountdirectives.h
+++ SalaryCount/SalaryCount/salarycountdirectives.h
@@ -7,13 +7,15 @@ class salarycountDirectives : public delegateStates
 {
     Q_OBJECT
 
+
 public:
 	salarycountDirectives(Ui_SalaryCount *ui, QString name="");
 	~salarycountDirectives();
-
-
 private:
-    
+    Ui_SalaryCount *ui;//не самый при€тный способ, зато все видно из коробки
+
+public slots:
+	void updateInfo(QString name);
 };
 
 #endif // SALARYCOUNTDIRECTIVES_H
