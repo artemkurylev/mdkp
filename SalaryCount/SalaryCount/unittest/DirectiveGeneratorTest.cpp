6bbd08335be57fdbea1d371f986513dbe8c78d5c
Ismodon <ismometrical@gmail.com>
2018-11-21 12:35:20 +0300

    changed my hiredirective method normal

--- SalaryCount/SalaryCount/unittest/DirectiveGeneratorTest.cpp
+++ SalaryCount/SalaryCount/unittest/DirectiveGeneratorTest.cpp
@@ -21,6 +21,7 @@ DirectiveGeneratorTest::~DirectiveGeneratorTest()
 //}
 
 
+<<<<<<< HEAD
 //void DirectiveGeneratorTest::normal()
 //{
 //	// create
@@ -28,3 +29,14 @@ DirectiveGeneratorTest::~DirectiveGeneratorTest()
 //	HireDirective hd(QDate(22,10,2018), QString("Ибрагим И.А."), PER_HOUR, 15000, 1234);
 //	generator.pdf( &hd, "normal.pdf");
 //}
+=======
+
+void DirectiveGeneratorTest::normal()
+{
+	// create
+	DirectiveGenerator generator;
+	HireDirective hd(5,QDate(22,10,2018), QString("Ибрагим И.А."), PER_HOUR, 15000, 1234);
+	generator.pdf( &hd, "normal.pdf");
+}
+
+>>>>>>> 949f7c3... changed my hiredirective method normal
