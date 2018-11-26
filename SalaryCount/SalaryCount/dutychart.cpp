ef117af16de2bb4bcf57511dbd44b53ed0fe8101
den1s0v <hello-lumen@ya.ru>
2018-11-26 11:04:01 +0300

    отмена коммита

--- SalaryCount/SalaryCount/dutychart.cpp
+++ SalaryCount/SalaryCount/dutychart.cpp
@@ -9,7 +9,6 @@ DutyChart::DutyChart(int id)
     : DbRecord(id)
 {
     //_id = id;
-		/* DbRecord(id) performs: assign id, fetch. */
 }
 DutyChart::DutyChart(const QString& name, const QList<Mark>& marks,const QDate& anchorDate, enum PayForm payForm/*=PER_HOUR*/)	
 {
