a3a418ce07ff8ea861c52526701449aabaa1b8d7
Artem Kurylev <artembonapart@gmail.com>
2018-12-03 17:35:41 +0400

    Добавлено открытие окна выбора предприятия при открытии приложения

--- SalaryCount/SalaryCount/main.cpp
+++ SalaryCount/SalaryCount/main.cpp
@@ -1,7 +1,7 @@
 #include "salarycount.h"
 #include <QtWidgets/QApplication>
 
-
+#include "companyopendialog.h"
 #include "DirectiveGenerator.h"
 
 
@@ -18,6 +18,8 @@ int main(int argc, char *argv[])
 
     QApplication a(argc, argv);
     SalaryCount w;
+    companyOpenDialog q;
+    q.show();
     w.show();
     return a.exec();
 }
