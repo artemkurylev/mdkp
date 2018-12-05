96be59b3e5072a92febada59bd1d7b98f5aefcb6
Artem Kurylev <artembonapart@gmail.com>
2018-12-05 13:42:09 +0400

    Добавлен метод для аутентификации предприятия

--- SalaryCount/SalaryCount/company.cpp
+++ SalaryCount/SalaryCount/company.cpp
@@ -1,5 +1,6 @@
 #include "company.h"
-
+#include <QCryptographicHash>
+#include <qbytearray.h>
 Company::Company(QObject *parent)
     : DbRecord(0)
 {
@@ -11,7 +12,7 @@ bool Company::createTable()
     if(DbManager::manager().checkConnection())
     {
         QSqlQuery* query = DbManager::manager().makeQuery();
-        if(query->exec("CREATE TABLE IF NOT EXISTS `company` (`id` INT(11) NOT NULL AUTO_INCREMENT, `name` VARCHAR(30) UNIQUE"))
+        if(query->exec("CREATE TABLE IF NOT EXISTS `company` (`id` INT(11) NOT NULL AUTO_INCREMENT, `name` VARCHAR(30) UNIQUE, `pass` VARCHAR(200)"))
 		{
             success = true;
 		}
@@ -25,6 +26,31 @@ bool Company::createTable()
     else
     {
     }
+    return success;
+}
+bool Company::auth()
+{
+    bool success = false;
+    QByteArray hash;
+    hash.append(this->_pass);
+    QString hsh = QCryptographicHash::hash(hash,QCryptographicHash::Md5);
+    QSqlQuery* query = DbManager::manager().makeQuery();
+    if(DbManager::manager().checkConnection())
+    {
+        if(query->exec("SELECT * FROM `company` WHERE `name` == :name"))
+        {
+            if(query->next())
+            {
+                QString pass = query->value(2).toString();
+                if(pass == hash)
+                {
+                    success = true;
+                }
+            }
+
+        }
+    }
+
     return success;
 }
 Company::~Company()
