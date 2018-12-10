92af6284b72fda41679938906e9bb931d00a9fec
Sereja Kalinkin <IKaSeI@yandex.ru>
2018-12-10 11:27:51 +0400

    Добавлена реализация метода подключения к базе данных

--- SalaryCount/SalaryCount/employeesc.cpp
+++ SalaryCount/SalaryCount/employeesc.cpp
@@ -18,3 +18,60 @@ EmployeeSC::~EmployeeSC()
 {
 	
 }
+
+void EmployeeSC::initialDBManager()
+{
+	DbManager& manager = DbManager::manager();
+    if(manager.checkConnection())
+    {
+        //Создание таблиц
+        bool table_created = Employee::createDbTable();
+        if(!table_created)
+        {
+
+        }
+        table_created = BillingPeriod::createDbTable();
+        if(!table_created)
+        {
+
+        }
+        table_created = DutyChart::createDbTable();
+        if(!table_created)
+        {
+
+        }
+        table_created = LaborSheet::createDbTable();
+        if(!table_created)
+        {
+
+        }
+        table_created = HireDirective::createDbTable();
+        if(!table_created)
+        {
+
+        }
+		// должно быть вызвано после создания DutyChart и LaborSheet (из-за внешних ключей)
+        table_created = Mark::createDbTable();
+        if(!table_created)
+        {
+
+        }
+        
+		// позже это должно выполняться при создании предприятия
+		initalSetupForTableDutyChart();
+    }
+	else
+	{
+		//TODO
+	}
+}
+
+Employee* EmployeeSC::shapeDataObject()
+{
+	return NULL;
+}
+
+void EmployeeSC::parseDataObject(Employee *obj)
+{
+
+}
