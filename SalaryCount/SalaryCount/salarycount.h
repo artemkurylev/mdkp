d3c1a0c139762ac3571553b30e19c0b6d2105fa9
Artem Kurylev <artembonapart@gmail.com>
2018-12-12 20:44:14 +0400

    Добавлен констркутор окна директора для кастомной дб

--- SalaryCount/SalaryCount/salarycount.h
+++ SalaryCount/SalaryCount/salarycount.h
@@ -22,6 +22,7 @@ private:
 
 public:
     SalaryCount(QWidget *parent = 0);
+    SalaryCount(QString dbName, QWidget *parent = 0);
     ~SalaryCount();
 
 private:
@@ -40,6 +41,7 @@ private:
 
 	//конструкторы
 	void initialDBManager();
+    void initialDBManager(QString dbName);
 
 signals:
 	void cancelChanges();
