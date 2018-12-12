5a5c802243f85c8973433aab2cd5e6d8a2a072e1
Artem Kurylev <artembonapart@gmail.com>
2018-12-12 19:47:30 +0400

    Добавлено отображение компаний

--- SalaryCount/SalaryCount/companyopendialog.cpp
+++ SalaryCount/SalaryCount/companyopendialog.cpp
@@ -4,7 +4,15 @@ companyOpenDialog::companyOpenDialog(QWidget *parent)
     : QDialog(parent)
 {
     ui.setupUi(this);
-    
+    initialDBManager();
+    if(!Company::countEntries())
+    {
+        initializeCompany();
+    }
+    QMap<int,QString> companies = Company::getAll();
+    for(auto it = companies.begin(); it != companies.end(); ++it){
+        ui.CompanyCombo_2->addItem(it.value(),it.key());
+    }
 }
 
 companyOpenDialog::~companyOpenDialog()
