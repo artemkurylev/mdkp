217a027cc002166d82562e62e69810014c04b83a
Artem Kurylev <artembonapart@gmail.com>
2018-12-12 19:28:28 +0400

    Добавлены  методы getAll, countEntries и метод для инициализации компании

--- SalaryCount/SalaryCount/company.h
+++ SalaryCount/SalaryCount/company.h
@@ -11,21 +11,24 @@ public:
     Company(QObject *parent = 0);
     Company(QString name,QString pass){_name=name;_pass=pass;}
     ~Company();
+
     //Getter
     QString name(){return this->_name;}
-    //Наследуемые методы
+    //Наследуемые метод
     int insert();
     bool update() const{return false;}
     bool fetch(){return false;}
     bool validate() const{return false;}
     //Статические методы
-    static QMap<int,QString> getAll(){QMap<int,QString> records; return records;}
+    static int countEntries();
+    static QMap<int,QString> getAll();
     static bool createTable();
     bool auth();
+
     
 private:
     QString _name;
     QString _pass;
 };
-
+void initializeCompany();
 #endif // COMPANY_H
