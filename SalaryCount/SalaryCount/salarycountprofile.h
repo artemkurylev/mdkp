05fd412cc2ffac2e162dc2b7ebdf5e360bbc4409
Artem Kurylev <artembonapart@gmail.com>
2018-12-13 13:31:51 +0400

    Добавлен констркутор и слот updateInfo

--- SalaryCount/SalaryCount/salarycountprofile.h
+++ SalaryCount/SalaryCount/salarycountprofile.h
@@ -1,21 +1,23 @@
 #ifndef SALARYCOUNTPROFILE_H
 #define SALARYCOUNTPROFILE_H
 
-#include <delegateStates>
+#include "delegateStates.h"
+
 
 class SalaryCountProfile : public delegateStates
 {
     Q_OBJECT
 
 public:
-    SalaryCountProfile(QObject *parent);
+    SalaryCountProfile(Ui_SalaryCount *ui, QString name="");
     ~SalaryCountProfile();
     
 
 private:
     Ui_SalaryCount *ui;//не самый при€тный способ, зато все видно из коробки
 
-
+public slots:
+	void updateInfo(QString name);
 };
 
 #endif // SALARYCOUNTPROFILE_H
