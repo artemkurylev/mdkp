abcea2b2a2afc4805723445200176b942cc13bee
Artem Kurylev <artembonapart@gmail.com>
2018-12-13 09:55:55 +0400

    Добавлена возможность создания предприятия

--- SalaryCount/SalaryCount/companycreationdialog.cpp
+++ SalaryCount/SalaryCount/companycreationdialog.cpp
@@ -1,12 +1,34 @@
 #include "companycreationdialog.h"
 
+
 companyCreationDialog::companyCreationDialog(QWidget *parent)
     : QDialog(parent)
 {
     ui.setupUi(this);
+
+    connect(ui.buttonBox->button(ui.buttonBox->Ok),SIGNAL(clicked()),this,SLOT(createCompany()));
 }
 
 companyCreationDialog::~companyCreationDialog()
 {
 
+}
+
+void companyCreationDialog::createCompany()
+{
+    QString name = ui.CompanyNameEdit->text();
+    QString pass_first = ui.passEdit->text();
+    QString pass_repeat = ui.passRepeatEdit->text();
+    if(pass_first != pass_repeat)
+    {
+        return;
+    }
+    Company company(name,pass_first);
+    //if(company.validate())
+    if(company.insert())
+    {
+        companyOpenDialog* open = new  companyOpenDialog();
+        this->close();
+        open->show();
+    }
 }
