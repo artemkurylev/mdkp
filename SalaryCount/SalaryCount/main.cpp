25c9d1bddf24826045e47c125eefb75afdb06520
den1s0v <hello-lumen@ya.ru>
2018-12-17 17:05:18 +0400

    Перенаправление стандартных потоков в файлы

--- SalaryCount/SalaryCount/main.cpp
+++ SalaryCount/SalaryCount/main.cpp
@@ -1,27 +1,52 @@
 #include "salarycount.h"
 #include "employeesc.h"
 #include <QtWidgets/QApplication>
+#include <QtCore/QCoreApplication>
 
 #include "companyopendialog.h"
-#include "DirectiveGenerator.h"
+#include "unittest/DirectiveGeneratorTest.h"
 #include <CalcTest.h>
 
+int test_main(int argc, char *argv[]);
 
 int main(int argc, char *argv[])
 {
+	if(true) // if tests enabled
+	{
+		return test_main(argc, argv);
+	}
 
 	//DirectiveGenerator g;
-	//HireDirective hd();
+	//HireDirective hd;
 
 	//g.pdf( &hd, "example.pdf" );
 	
-	/*DirectiveGeneratorTest dgt(0);
-	QTest::qExec( &dgt , NULL , NULL);*/
-
+	//DirectiveGeneratorTest dgt(0);
+	//QTest::qExec( &dgt , NULL , NULL);
+	
     QApplication a(argc, argv);
     companyOpenDialog q;
     q.show();
     //SalaryCount w;
     //w.show();
     return a.exec();
+}
+
+int test_main(int argc, char *argv[])
+{
+    QCoreApplication a(argc, argv);
+
+	freopen ("sc_stderr.log","w",stderr);
+	freopen ("sc_stdout.log","w",stdout);
+	
+
+	fprintf (stderr, "This sentence is redirected to a file.");
+	printf ("This sentence is redirected to a file.");
+
+	CalcTest t;
+	QTest::qExec( &t , NULL , NULL);
+
+
+	fclose (stderr);
+    return 0; // a.exec();
 }
