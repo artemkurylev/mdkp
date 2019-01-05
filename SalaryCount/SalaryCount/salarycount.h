ee6c15cb920698fff186f126555ba48ad607258d
den1s0v <hello-lumen@ya.ru>
2019-01-06 00:31:32 +0400

    Привязка запуска тестов по сочетанию клавиш Ctrl+T

--- SalaryCount/SalaryCount/salarycount.h
+++ SalaryCount/SalaryCount/salarycount.h
@@ -52,6 +52,9 @@ public slots:
 	//! переходы по страницам приложения
 	void showPage(QAction* actionEmited);
 
+	//! Тестирование
+	void startTesting();
+
 private slots:
 	void rememberState(bool state);
 	//! меню "Файл"
