7c8433ce7f54be19d480f15ca05df184ccc1ad79
Sereja Kalinkin <IKaSeI@yandex.ru>
2019-01-15 18:46:50 +0400

    Добавлен класс взаимодействия для создания компании
    # Conflicts:
    #	SalaryCount/SalaryCount/SalaryCount.vcxproj

---
+++ SalaryCount/SalaryCount/companycreationdialog.cpp
@@ -0,0 +1,12 @@
+#include "companycreationdialog.h"
+
+companyCreationDialog::companyCreationDialog(QWidget *parent)
+    : QDialog(parent)
+{
+    ui.setupUi(this);
+}
+
+companyCreationDialog::~companyCreationDialog()
+{
+
+}
